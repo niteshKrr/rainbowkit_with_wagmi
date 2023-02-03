fa547e3617c1b19d2704d9a01720d9e465f1cbac
niteshKrr <nkumar35101@gmail.com>
2023-02-03 15:50:39 +0530

    different custamization of rainbowkit exploted

---
+++ src/hooks/useIsMounted.ts
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState,useEffect  } from 'react'
+
+const useIsMounted = () => {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, [])
+    
+  return mounted;
+}
+
+export default useIsMounted
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider,darkTheme, Chain } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, goerli, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, goerli],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID || "" }),
+    //  publicProvider()
+    ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider initialChain={mainnet}
+       showRecentTransactions={true}
+        chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
